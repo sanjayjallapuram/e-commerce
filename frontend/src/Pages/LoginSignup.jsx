8d3430407434fc9958f3c3e118f4e6d0d3d1936c
sanjayjallapuram <137622964+sanjayjallapuram@users.noreply.github.com>
2024-07-26 13:43:05 +0530

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -17,7 +17,7 @@ export default function LoginSignup(){
    const login = async()=>{
       console.log("Login function executed",formData);
       let responseData;
-      await fetch('http://localhost:4000/login',{
+      await fetch('https://e-commerce-backend-x2d8.onrender.com',{
          method:'POST',
          headers:{
             Accept:'application/form-data',
@@ -38,7 +38,7 @@ export default function LoginSignup(){
    const signup = async()=>{
       console.log("signup function executed",formData);
       let responseData;
-      await fetch('http://localhost:4000/signup',{
+      await fetch('https://e-commerce-backend-x2d8.onrender.com',{
          method:'POST',
          headers:{
             Accept:'application/form-data',
