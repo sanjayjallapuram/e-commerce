03f43af8141e36ac4722096ef6d8c19fbe3ece6a
sanjayjallapuram <137622964+sanjayjallapuram@users.noreply.github.com>
2024-07-26 13:40:05 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -29,7 +29,7 @@ export default function AddProduct(){
         let formData = new FormData();
         formData.append('product',image);
 
-        await fetch('http://localhost:4000/upload',{
+        await fetch('https://e-commerce-backend-x2d8.onrender.com',{
             method:'POST',
             headers:{
                 Accept:'application/json'
@@ -40,7 +40,7 @@ export default function AddProduct(){
         if(responseData.success){
             product.image = responseData.image_url;
             console.log(product)
-            await fetch('http://localhost:4000/addproduct',{
+            await fetch('https://e-commerce-backend-x2d8.onrender.com',{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
