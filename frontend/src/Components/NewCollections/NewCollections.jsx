3603aaf857bcc01fddad47c7bdee558891584923
sanjayjallapuram <137622964+sanjayjallapuram@users.noreply.github.com>
2024-07-26 14:16:19 +0530

    Update NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -6,7 +6,7 @@ export default function NewCollections(){
     const [new_collection,setNew_collection] = useState([]);
     
     useEffect(()=>{
-        fetch('http://localhost:4000/newcollection')
+        fetch('https://e-commerce-backend-x2d8.onrender.com')
         .then((response)=>response.json())
         .then((data)=>setNew_collection(data));
     },[])
